76f643896b814b5635efa71d5d04e4ac9ba1d577
yanghyunw00 <103232234+yanghyunw00@users.noreply.github.com>
2022-06-15 23:13:09 +0900

    finalexam
    finalexam

---
+++ enemy.js
@@ -0,0 +1,37 @@
+class Enemy { //right to left
+    constructor() {
+        this.r = 100;
+        this.x = width+100;
+        this.y = height - this.r;
+        this.rand = int(random(4));
+        console.log(this.rand);
+        if(this.rand == 0) { this.velocityX = random(2,4) } 
+        else if(this.rand == 1) { this.velocityX = -random(2,4) } 
+        
+    }
+
+    move() {
+       this.x -= this.velocityX
+
+        if(this.x > width) {
+            this.x -= this.velocityX;
+        }
+    }
+
+    show() {
+        image(elf2,this.x, this.y, this.r, this.r)
+ //hit box       
+        // fill(255, 50);
+        // ellipseMode(CORNER);
+        // ellipse(this.x+10, this.y+10, this.r-40, this.r);
+    }
+
+    reset() {
+        this.r = 100;
+        this.x = width;
+        this.y = height - this.r;
+        this.rand = int(random(4));
+        if(this.rand == 0) { this.velocityX = random(2,4) } 
+        else if(this.rand == 1) { this.velocityX = -random(2,4)} 
+    }   
+}
